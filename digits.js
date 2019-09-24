f96c5d3b930e6b829220538d9d7e46463e90fa3a
Jason <velojason@users.noreply.github.com>
2019-09-24 16:19:17 -0700

    Update digits.js

--- digits.js
+++ digits.js
@@ -1,5 +1,4 @@
 // Split a number into an array of its digits.
-
 const digits = (arr) => {
   // Could be:
   return `${arr}`.split('').map(x => parseFloat(x));
@@ -8,5 +7,16 @@ const digits = (arr) => {
   return String(arr).split('').map(x => Number(x));
 }
 
-// Find the largest sum of two digits in a number.
-// TODO: add function.
+// Find the largest sum elements in an array.
+const subarray = (arr) => {
+  let accumulator = 0;
+  let largest = Number.NEGATIVE_INFINITY;
+  
+  for (let i = 0, l = arr.length; i < l; i++) {
+    accumulator = Math.max(arr[i], arr[i] + accumulator);
+    largest = Math.max(accumulator, largest);
+  }
+  return largest;
+}
+
+subarray([-2, 1, -3, 4, 3, -2, 1, -5, 4]); // 7
